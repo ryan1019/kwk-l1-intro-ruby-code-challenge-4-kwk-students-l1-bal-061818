5b1c2c4d9c77208c482bbad85ee69d4daa04e3ac
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-22 13:08:29 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,10 +2,19 @@
 
 def least_coins(cents)
   coin_type = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
-  while cents >= 25
+  while cents == 25
+    puts coin_type
     least_coins[:quarters] += 1
+  end 
+
+while cents == 10 
+  puts coin_type 
+  least_coins[:dimes] += 1 
+end 
+
+
     
-end
+
 
 
 
